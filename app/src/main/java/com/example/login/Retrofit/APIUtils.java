ebdda56834defeb961d216420040767fc605c61a
pntuan <ntdcap1@gmail.com>
2019-12-05 11:59:58 +0700

    grade

--- app/src/main/java/com/example/login/Retrofit/APIUtils.java
+++ app/src/main/java/com/example/login/Retrofit/APIUtils.java
@@ -4,7 +4,7 @@ public class APIUtils {
 //    public static final String Base_Url="http:172.30.99.190:8000";
 
 //public static final String Base_Url="http://192.168.240.1:8000";
-public static final String Base_Url="http://192.168.223.57:8000";
+public static final String Base_Url="http://172.30.174.128:8000";
 
 
 
